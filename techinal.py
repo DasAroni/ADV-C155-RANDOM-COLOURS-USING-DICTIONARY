1aebec92b8d48caa0f2c08c0fe51f721445e06a6
DasAroni <107314887+DasAroni@users.noreply.github.com>
2023-02-11 12:06:23 +0530

    Add files via upload

---
+++ techinal.py
@@ -0,0 +1,23 @@
+from tkinter import *
+root = Tk()
+
+root.title("Tecnical Dictionary")
+root.geometry("500x500")
+root.configure(background="orange","red","yrllow")
+
+
+
+
+
+
+
+
+
+btn_2=Button(root,text="",bg="yellow",fg="Black",)
+btn_2.place(relx =0.5,rely = 0.5,anchor=CENTER )
+
+
+
+
+
+root.mainloop()
